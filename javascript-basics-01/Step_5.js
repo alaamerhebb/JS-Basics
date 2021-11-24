014e730d59a04fd9c5b0f881fdd4bd78791c6626
alaamerhebb <alaamerhebb@gmail.com>
2021-11-24 23:58:04 +0200

    completed js-basics-01 step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function multipy() {
+var firstnumber = document.getElementById("first_number").value;
+var secondnumber = document.getElementById("second_number").value;
+
+alert ("The answer is:" +"\n" + firstnumber * secondnumber);
+}
