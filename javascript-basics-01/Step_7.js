38ee5987065ed6cbebe39273ee7178d8447b818b
alaamerhebb <alaamerhebb@gmail.com>
2021-11-25 00:39:26 +0200

    completed js-basics-01 step_7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,6 @@
+function operation() {
+  let x = document.getElementById("shoe_size").value;
+  let y = document.getElementById("year").value;
+  let z = (x*2 +5)*50 -y +1766 ;
+  alert("the result is: " +"\n"+ z)
+}
