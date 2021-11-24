261ff998341b5ff9c9a31fd1b746ce30a4a671ab
alaamerhebb <alaamerhebb@gmail.com>
2021-11-24 22:34:56 +0200

    completed js-basics-01 step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("enter you name here...");
+var surname = prompt("enter you surname here...");
+var city = prompt("enter your city here...?");
+
+alert ("name : " +name +"\nsurname : " +surname +"\ncity : " +city );
