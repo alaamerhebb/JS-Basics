7aca65bf6d06a7365d950ba1ac5cec6dffe7ce7b
alaamerhebb <alaamerhebb@gmail.com>
2021-11-25 00:17:56 +0200

    completed js-basics-01 step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function divise() {
+    var firstnumber = document.getElementById("first_number").value;
+    var secondnumber = document.getElementById("second_number").value;
+    var result = firstnumber / secondnumber;
+    alert ("the reminder of devision is:" +"\n" + result);
+    }
