40c74a72a6bfc103dc41b5f5aa5d931ec89f3d8c
alaamerhebb <alaamerhebb@gmail.com>
2021-12-05 19:47:20 +0200

    completed js-basics-03 step_2

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,7 @@
+function change(num) {
+    if (num == 0) {
+        document.getElementById("texte").style.display = "none";
+    } else {
+        document.getElementById("texte").style.display = "";
+    }
+} 
