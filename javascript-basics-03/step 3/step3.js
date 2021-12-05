d03b9b333d93478cd5742d915b4fd3dd8bf1b5a7
alaamerhebb <alaamerhebb@gmail.com>
2021-12-05 20:08:41 +0200

    completed js-basics-03 step_3

---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,12 @@
+text = document.getElementById("text");
+function color(num) {
+    if (num==1) {
+      text.style.color="green";
+    }
+    if (num==2) {
+        text.style.color="red";
+      }
+      if (num==3) {
+        text.style.color="blue";
+      }
+}
