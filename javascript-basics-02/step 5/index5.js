4a6bd4a6afd5e844a3f9e02388a30d89c1c7ec8d
alaamerhebb <alaamerhebb@gmail.com>
2021-12-05 18:31:10 +0200

    completed js-basics-02 step_5

---
+++ javascript-basics-02/step 5/index5.js
@@ -0,0 +1,7 @@
+var Image = document.querySelectorAll("img");
+
+for (let i = 0; i < Image.length; i++) {
+    Image[i].addEventListener("mouseover", function() {
+        document.getElementById("image" + (i + 1)).src = "images/image" + (i + 1) + "_2.jpg"
+    })
+} 
